2cee618f92cd9e6419b8c97b5ee9ebfabd59b2f6
leyla <leyla.lgc99@gmail.com>
2021-12-06 23:01:34 -0500

    configuración inicial

---
+++ SistemaHospital/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ SistemaHospital/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBMQs4o7_CwjX80sQsmQtVLIv9NCuieEXA",
+    authDomain: "sistemahospitalario-8ad0d.firebaseapp.com",
+    projectId: "sistemahospitalario-8ad0d",
+    storageBucket: "sistemahospitalario-8ad0d.appspot.com",
+    messagingSenderId: "596047175290",
+    appId: "1:596047175290:web:b985e1f560e590933a25c3",
+    measurementId: "G-0G530TW29S"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
